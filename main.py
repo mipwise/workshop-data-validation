e644b54cd876a0c1d262e30732daeea65a4e6b01
Andrea Rujano <andrea.jenir@mipwise.com>
2023-03-06 20:35:46 -0300

    fix: add reset_index to bring missing columns before merge

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ def data_validation(_dat):
 
 
 def compute_price_extremes(_dat):
-    price_tiers_extremes = _dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).agg({'Tier End': 'max', 'Price': 'min'}).\
-        rename(columns={'max': 'Price Tier Max. Value', 'min': 'Price Tier Minimum Cost'})
-    price_tiers_extremes_df = _dat.skus[['SKU ID', 'SKU Name']].merge(price_tiers_extremes, on='SKU ID', how='inner')
+    # SOLVE: why this is wrong
+    # price_tiers_extremes = _dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).agg({'Tier End': 'max', 'Price': 'min'}).\
+    #     reset_index().rename(columns={'Tier End': 'Price Tier Max. Value', 'Price': 'Price Tier Minimum Cost'})
+    price_tiers_extremes = _dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).agg({'Tier End': 'max', 'Price': 'min'}). \
+        reset_index().rename(columns={'Tier End': 'Max. Price Tier Value', 'Price': 'Min. Price Tier Cost'})
+    price_tiers_extremes_df = _dat.skus[['SKU ID', 'SKU Name']].merge(price_tiers_extremes, on='SKU ID', how='left')
     price_tiers_extremes_df = _dat.suppliers[['Supplier ID', 'Supplier Name']].merge(price_tiers_extremes_df,
                                                                                      on='Supplier ID', how='inner')
     _sln = output_schema.PanDat()
     _sln.price_tiers_extremes = price_tiers_extremes_df
-    return sln
-
-
+    return _sln
 
 
 if __name__ == '__main__':
-    dat = utils.read_data(os.path.join('data.xlsx'), input_schema)
+    dat = utils.read_data(os.path.join('data_modified.xlsx'), input_schema)
     data_validation(dat)
+    # utils.write_data(dat, 'input', input_schema) # optional
     sln = compute_price_extremes(dat)
-    utils.write_data(sln, 'price_tiers_extremes.csv', output_schema)
+    utils.write_data(sln, 'price_tiers_extremes.xlsx', output_schema)
