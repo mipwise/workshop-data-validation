9e435a71a9d0cb2f0de3e50c59d31fcb8c29a069
Aster Santana <50426897+astersantana@users.noreply.github.com>
2023-03-06 21:08:21 -0300

    refactor: rename some files and add documentation in the code

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ import utils
 import os
 
 
-def data_validation(_dat):
-    utils.check_data(_dat, input_schema)
+def compute_price_extremes(dat):
+    """Simulates a solution engine that reads takes an input data instance, performs some simple calculations, and
+    populates the output schema with the results."""
 
+    # perform computations
+    price_tiers_extremes_df = dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).\
+        agg({'Tier End': 'max', 'Price': 'min'}).\
+        reset_index().\
+        rename(columns={'Tier End': 'Max. Price Tier Value', 'Price': 'Min. Price Tier Cost'})
 
-def compute_price_extremes(_dat):
-    # SOLVE: why this is wrong
-    # price_tiers_extremes = _dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).agg({'Tier End': 'max', 'Price': 'min'}).\
-    #     reset_index().rename(columns={'Tier End': 'Price Tier Max. Value', 'Price': 'Price Tier Minimum Cost'})
-    price_tiers_extremes = _dat.price_tiers.groupby(['SKU ID', 'Supplier ID']).agg({'Tier End': 'max', 'Price': 'min'}). \
-        reset_index().rename(columns={'Tier End': 'Max. Price Tier Value', 'Price': 'Min. Price Tier Cost'})
-    price_tiers_extremes_df = _dat.skus[['SKU ID', 'SKU Name']].merge(price_tiers_extremes, on='SKU ID', how='left')
-    price_tiers_extremes_df = _dat.suppliers[['Supplier ID', 'Supplier Name']].merge(price_tiers_extremes_df,
-                                                                                     on='Supplier ID', how='inner')
-    _sln = output_schema.PanDat()
-    _sln.price_tiers_extremes = price_tiers_extremes_df
-    return _sln
+    # populate SKU and supplier names from master data
+    price_tiers_extremes_df = dat.skus[['SKU ID', 'SKU Name']].\
+        merge(price_tiers_extremes_df, on='SKU ID', how='left')
+    price_tiers_extremes_df = dat.suppliers[['Supplier ID', 'Supplier Name']].\
+        merge(price_tiers_extremes_df, on='Supplier ID', how='inner')
+
+    # populate output schema
+    sln = output_schema.PanDat()
+    sln.price_tiers_extremes = price_tiers_extremes_df
+    return sln
 
 
 if __name__ == '__main__':
-    dat = utils.read_data(os.path.join('data.xlsx'), input_schema)
-    data_validation(dat)
-    # utils.write_data(dat, 'input', input_schema) # optional
-    sln = compute_price_extremes(dat)
-    utils.write_data(sln, 'price_tiers_extremes.xlsx', output_schema)
+    _dat = utils.read_data(os.path.join('data.xlsx'), input_schema)
+    utils.check_data(_dat, input_schema)
+    _sln = compute_price_extremes(_dat)
+    utils.write_data(_sln, 'price_tiers_extremes.xlsx', output_schema)
